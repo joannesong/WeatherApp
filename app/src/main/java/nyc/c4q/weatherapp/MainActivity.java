60be1cf37898e8cd6e2df518d6134a881c820217
Jervon Arnold <jervonjarnold@gmail.com>
2018-02-06 16:14:58 -0500

    issue with notifications

--- app/src/main/java/nyc/c4q/weatherapp/MainActivity.java
+++ app/src/main/java/nyc/c4q/weatherapp/MainActivity.java
@@ -1,8 +1,17 @@
 package nyc.c4q.weatherapp;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import nyc.c4q.weatherapp.network.API;
 import nyc.c4q.weatherapp.model.WeatherPOJO;
@@ -13,39 +22,80 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
+    Retrofit retrofit;
+    String id = "Mbfz6KHEyqiIF93hy5XRj";
+    String secret = "I7jQI5udlLdLO6N9XQ9mPzRRBppwaN8XznscuLNs";
+    private static final int NOTIFICATION_ID = 555;
+    String hello = "testing";
+    String NOTIFICATION_CHANNEL = "C4Q Notifications";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        setUP();
+//        launchTestService();
+//        mNotifyManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendNotification();
+            }
+        });
 
 
-        String id="Mbfz6KHEyqiIF93hy5XRj";
-        String secret="I7jQI5udlLdLO6N9XQ9mPzRRBppwaN8XznscuLNs";
+    }
 
-        Retrofit retrofit=new Retrofit.Builder()
+    public void sendNotification() {
+        NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        String id = "my_channel_01";
+        CharSequence name = getString(R.string.channel_name);
+        String description = getString(R.string.channel_description);
+        int importance = NotificationManager.IMPORTANCE_LOW;
+        NotificationChannel mChannel = new NotificationChannel(id, name, importance);
+        mChannel.setDescription(description);
+        mChannel.enableLights(true);
+        mChannel.setLightColor(Color.RED);
+        mChannel.enableVibration(true);
+        mChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+
+        mNotificationManager.createNotificationChannel(mChannel);
+
+    }
+
+    public void setUP() {
+        retrofit = new Retrofit.Builder()
                 .baseUrl("https://api.aerisapi.com/forecasts/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
+        networkCall();
+    }
 
-        API api =retrofit.create(API.class);
-        Call<WeatherPOJO> call = api.getForcast(id,secret);
+    public void networkCall() {
+        API api = retrofit.create(API.class);
+        Call<WeatherPOJO> call = api.getForcast(id, secret);
         call.enqueue(new Callback<WeatherPOJO>() {
             @Override
             public void onResponse(Call<WeatherPOJO> call, Response<WeatherPOJO> response) {
-               if (response.isSuccessful()){
-                   WeatherPOJO forcast =response.body();
-                       Log.e("Successessful", forcast.getResponse().get(0).getPeriods().get(0).getIcon()+"");
-               }
+                if (response.isSuccessful()) {
+                    WeatherPOJO forcast = response.body();
+                    Log.e("Successessful", forcast.getResponse().get(0).getPeriods().get(0).getIcon() + "");
+                }
 
 
             }
 
             @Override
             public void onFailure(Call<WeatherPOJO> call, Throwable t) {
-                Log.e("Failed",t.getMessage());
+                Log.e("Failed", t.getMessage());
             }
         });
+    }
 
+    public void launchTestService() {
+        Intent i = new Intent(this, MyIntentService.class);
+        startService(i);
     }
 }
