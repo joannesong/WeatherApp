0620b0a815ecc09f2852ac3e7a0658ba0649fa9b
Jervon Arnold <jervonjarnold@gmail.com>
2018-02-05 19:40:39 -0500

    deleted my pojo used Joann's model instead

--- app/src/main/java/nyc/c4q/weatherapp/network/API.java
+++ app/src/main/java/nyc/c4q/weatherapp/network/API.java
@@ -1,11 +1,8 @@
 package nyc.c4q.weatherapp.network;
 
-import nyc.c4q.weatherapp.model.Forcast;
-import okhttp3.ResponseBody;
+import nyc.c4q.weatherapp.model.WeatherPOJO;
 import retrofit2.Call;
-import retrofit2.Response;
 import retrofit2.http.GET;
-import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 /**
@@ -15,5 +12,5 @@ import retrofit2.http.Query;
 public interface API {
 
     @GET("/forecasts/11212")
-    Call<Forcast> getForcast(@Query("client_id") String client_id, @Query("client_secret") String secret);
+    Call<WeatherPOJO> getForcast(@Query("client_id") String client_id, @Query("client_secret") String secret);
 }
