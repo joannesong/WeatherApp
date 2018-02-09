cdce36a45a970cde400a08c08aabb3df693b6971
Joanne Song <joannesong@ac.c4q.nyc>
2018-02-08 20:25:34 -0500

    adding JobScheduler

--- app/src/main/java/nyc/c4q/weatherapp/MainActivity.java
+++ app/src/main/java/nyc/c4q/weatherapp/MainActivity.java
@@ -46,8 +46,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-
         setUP();
 
 //        mNotifyManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
